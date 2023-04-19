ee86b80677574c5fdfbd89eaf0638f5422c374e8
evgenyi <fonya93@yandex.ru>
2023-04-19 22:54:00 +0700

    Расстояние между 2 точками в 3D

---
+++ task21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Принимает на вход координаты 2х точек и находит расстояние между ними в 3D пространстве
+int x1 = GetUserInput("Введите координату х для точки А:");
+int y1 = GetUserInput("Введите координату y для точки А:");
+int z1 = GetUserInput("Введите координату z для точки А:");
+int x2 = GetUserInput("Введите координату х для точки B:");
+int y2 = GetUserInput("Введите координату y для точки B:");
+int z2 = GetUserInput("Введите координату z для точки B:");
+
+double tempX = Math.Pow(x2 - x1, 2);
+double tempY = Math.Pow(y2 - y1, 2);
+double tempZ = Math.Pow(z2 - z1, 2);
+double result = Math.Sqrt(tempX + tempY + tempZ);
+
+Console.WriteLine($"A({x1},{y1},{z1}) , B({x2},{y2},{z2}) -> {result:f2}");
+
+static int GetUserInput(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        int UserInput;
+        if (int.TryParse(Console.ReadLine(), out UserInput))
+            return UserInput;
+        else Console.WriteLine("Ошибка ввода");
+
+    }
+}
