f510157c28f48b550b6a31deff2bf7fd10a0c9cd
evgenyi <fonya93@yandex.ru>
2023-04-20 12:22:28 +0700

    Выводит числа N в кубе

---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿int N = GetUserInput("Введите число N:");
+
+Console.Write($"{N} -> ");
+for (int i=1; i<=N; i++)
+{
+    double result = Math.Pow(i,3);
+    Console.Write($"{result} ");
+}
+static int GetUserInput(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        int UserInput;
+        if (int.TryParse(Console.ReadLine(), out UserInput))
+            return UserInput;
+        else Console.WriteLine("Нужно ввести число !");
+
+    }
+}
