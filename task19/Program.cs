0b9209b3b808d93c96cc9cb25e94ef6f3ea3cc69
evgenyi <fonya93@yandex.ru>
2023-04-20 10:14:37 +0700

    Задача на проверку полиндрома

---
+++ task19/Program.cs
@@ -0,0 +1,28 @@
+﻿int number = GetUserInput("Введите пятизначное число:");
+
+int first = number / 10000 % 10;
+int second = number / 1000 % 10;
+int fourth = number / 10 % 10;
+int fifth = number % 10;
+
+if (number > 9999 && number < 100000)
+{
+    if (first == fifth && second == fourth)
+        Console.WriteLine($"{number} - > Полиндром");
+    else
+        Console.WriteLine($"{number} - > Не полиндром");
+}
+else
+    Console.WriteLine("Должно быть пять цифр !");
+static int GetUserInput(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        int UserInput;
+        if (int.TryParse(Console.ReadLine(), out UserInput))
+            return UserInput;
+        else Console.WriteLine("Нужно ввести число !");
+
+    }
+}
